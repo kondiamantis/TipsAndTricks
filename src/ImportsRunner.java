76528b6acf08d9cb42d02e4252e56ca7664b9cfb
IKNOWHOW\kdiamantis <kdiamantis@iknowhow.com>
2023-01-10 12:28:16 +0200

    Initial commit

---
+++ src/ImportsRunner.java
@@ -0,0 +1,21 @@
+import java.lang.String;
+import java.math.BigDecimal;
+import java.text.CollationElementIterator;
+import java.util.ArrayList;
+
+import static java.util.Collections.*;
+import static java.lang.System.out;
+
+
+public class ImportsRunner {
+    public static void main(String[] args) {
+        String str = "";
+        BigDecimal db = null;
+        out.println(); //Since i have made an import, i can use it this way
+
+        //Collections.sort(new ArrayList<String>());
+        sort(new ArrayList<String>());
+
+
+    }
+}
